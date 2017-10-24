a62a3d8ce4915d51b0ffeef0909ac67b2378985f
kongou-ae <y.matsumoto.ae@gmail.com>
2017-10-24 14:42:14 +0000

    add --no-perms

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -xe
-lftp -e "mirror -R --delete-first ./dist . &&exit" ftps://${webappName}:${webappPass}@waws-prod-ty1-005.ftp.azurewebsites.windows.net/site/wwwroot
+lftp -e "mirror -R --no-perms ./dist . &&exit" ftps://${webappName}:${webappPass}@waws-prod-ty1-005.ftp.azurewebsites.windows.net/site/wwwroot

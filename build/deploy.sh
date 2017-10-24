1d0e322ff4f6a6d0519ac32052887ba556c66356
kongou-ae <y.matsumoto.ae@gmail.com>
2017-10-24 14:34:12 +0000

    fixed CI

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -xe
-lftp -e "mirror -R ./dist . &&exit" -u ${webappAuth} ftps://waws-prod-ty1-005.ftp.azurewebsites.windows.net/site/wwwroot
+lftp -e "mirror -R ./dist . &&exit" ftps://${webappName}:${webappPass}@waws-prod-ty1-005.ftp.azurewebsites.windows.net/site/wwwroot
